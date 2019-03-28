5dd9f44356123cd0bbb058b452c370a13a3927dd
Rajat Silwal <rajatsilwal7@gmail.com>
2019-03-29 05:04:10 +0530

    added form to contact page

--- inc/mail_from.php
+++ inc/mail_from.php
@@ -1,22 +1,16 @@
 <form action="contact.php" method="POST">
 	<table>
 		<tr>
-			<h2>Add a T-shirts</h2>
-		</tr>
-		<tr>
-			<th><lable for="tshirt_pic">Upload image</lable></th>
-			<td><input type="file" name="tshirt_pic" accept="image/*"></td>
-		</tr>
-		<tr>
 			<th>Name</th>
-			<td><input type="text" name="name" placeholder="Long,red"></td>
+			<td><input type="text" name="name" placeholder="Rajat Silwal" required></td>
 		</tr>
 		<tr>
-			<th>Price</th>
-			<td><input type="text" name="price"></td>
+			<th>Email</th>
+			<td><input type="email" name="email" required></td>
 		</tr>
 		<tr>
-			<th>Sizes</th>
+			<th>Message</th>
+			<td><input type="textbox" name="Message" required></td>
 		</tr>
 	</table>
 	<input type="submit">
