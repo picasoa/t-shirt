14d7446aeab64aefe0ef2a78a64d55fc1bc04db8
Rajat Silwal <rajatsilwal7@gmail.com>
2019-03-29 05:10:49 +0530

    made changes in the form

--- inc/mail_from.php
+++ inc/mail_from.php
@@ -1,16 +1,16 @@
 <form action="contact.php" method="POST">
 	<table>
 		<tr>
-			<th>Name</th>
-			<td><input type="text" name="name" placeholder="Rajat Silwal" required></td>
+			<th><label for="name">Name</label></th>
+			<td><input type="text" name="name" placeholder="Rajat Silwal" id="name" required></td>
 		</tr>
 		<tr>
-			<th>Email</th>
-			<td><input type="email" name="email" required></td>
+			<th><lable for="email">Email</lable></th>
+			<td><input type="email" name="email" id="email" required></td>
 		</tr>
 		<tr>
-			<th>Message</th>
-			<td><input type="textbox" name="Message" required></td>
+			<th><lable for="message">Message</lable></th>
+			<td><textarea name="Message" id="message" required></textarea></td>
 		</tr>
 	</table>
 	<input type="submit">
